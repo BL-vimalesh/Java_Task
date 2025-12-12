e903ff3b10ef8a0683acdac6825ac87b77d6a791
BL-vimalesh <vimalesh.ravikumar@bridgelabz.com>
2025-12-12 17:20:34 +0530

    added day-12 task

--- Bridgelabz_task/Day-12/GenericMax.java
+++ Bridgelabz_task/Day-12/GenericMax.java
@@ -11,24 +11,14 @@ public class GenericMax {
         if (c.compareTo(max) > 0) {
             max = c;
         }
-
         return max;
     }
-
     public static void main(String[] args) {
 
         System.out.println("UC1: " + findMax(30, 20, 10)); // uc - 1
-
-
         System.out.println("UC2: " + findMax(10, 50, 20)); // uc -2
-
-
         System.out.println("UC3: " + findMax(10, 20, 90)); // uc - 2
-
- 
         System.out.println("Float: " + findMax(10.0f, 30.2f, 82.2f)); 
-
- 
         System.out.println("String: " + findMax("Apple", "Peach", "Banana")); 
         
     }
